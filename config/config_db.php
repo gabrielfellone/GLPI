47fb4069df4c00cae47ef92a105a7f494cf9dced
Gabriel <gabrielfellone@hotmail.com>
2019-01-10 11:28:18 -0500

    GLP_Dir

---
+++ config/config_db.php
@@ -0,0 +1,12 @@
+<?php
+ class DB extends DBmysql {
+                
+ public $dbhost     = '192.168.1.99';
+                
+ public $dbuser     = 'root';
+                
+ public $dbpassword = 'sistemas2112';
+                
+ public $dbdefault  = 'glpi';
+                
+}
